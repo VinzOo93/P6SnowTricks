1446de1a765d4b5fd13bc1a79ebd261707723cf6
VinzOo93 <v.orru@live.fr>
2022-01-13 00:01:44 +0100

    front for comment OK

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -14,7 +14,7 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('content', TextareaType::class, [
-                'label' => 'commentaire',
+                'label' => 'Commentaire',
                 'required' => true,
             ])
         ;
